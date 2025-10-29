305524a2169b7cc98e5db7cea6080c1531e8730b
lamine <laoufi.mohamed.lamine@gmail.com>
2025-10-29 01:26:57 +0100

    finsihing product-service crud operations

--- apps/product-service/src/controllers/category.controller.ts
+++ apps/product-service/src/controllers/category.controller.ts
@@ -23,7 +23,41 @@ export const updateCategory = async (
   res: Response
 ): Promise<void> => {
   try {
-    // Update category logic here
+    // Update category logic here const { id } = req.params;
+    const { id } = req.params;
+    if (!id || !Number(id)) {
+      res.status(400).json({
+        success: false,
+        message: "Bad Request, id is required to update category",
+      });
+      return;
+    }
+    const product = await prisma.product.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!product) {
+      res.status(404).json({
+        success: false,
+        message: "category Not Found",
+      });
+      return;
+    }
+    const data: Prisma.CategoryUpdateInput = req.body;
+    if (!data) {
+      res.status(400).json({
+        success: false,
+        message: "Bad Request, please provide data to update",
+      });
+      return;
+    }
+    await prisma.category.update({
+      where: {
+        id: Number(id),
+      },
+      data,
+    });
     res.status(200).json({ message: "category updated successfully" });
   } catch (error) {
     console.error(error);
@@ -36,7 +70,32 @@ export const deleteCategory = async (
   res: Response
 ): Promise<void> => {
   try {
-    // Delete category logic here
+    const { id } = req.params;
+    if (!id || !Number(id)) {
+      res.status(400).json({
+        success: false,
+        message: "Bad Request, id is required to delete category",
+      });
+      return;
+    }
+    const product = await prisma.product.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!product) {
+      res.status(404).json({
+        success: false,
+        message: "category Not Found",
+      });
+      return;
+    }
+    await prisma.category.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
     res.status(200).json({ message: "category deleted successfully" });
   } catch (error) {
     console.error(error);
@@ -49,8 +108,8 @@ export const getCategorys = async (
   res: Response
 ): Promise<void> => {
   try {
-    // Fetch categorys logic here
-    res.status(200).json({ data: [] });
+    const categories = await prisma.category.findMany();
+    res.status(200).json({ data: categories });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch categorys" });
@@ -62,8 +121,25 @@ export const getCategoryById = async (
   res: Response
 ): Promise<void> => {
   try {
-    // Fetch single category by ID logic here
-    res.status(200).json({ data: {} });
+    const { id } = req.params;
+    if (!id) {
+      res.status(401).json({ message: "please provide id" });
+      return;
+    }
+
+    const category = await prisma.category.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!category) {
+      res.status(404).json({
+        success: false,
+        message: "category Not Found",
+      });
+      return;
+    }
+    res.status(200).json({ data: { category } });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch category" });
--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -1,7 +1,18 @@
 import { Prisma, prisma } from "@repo/db";
 import { Request, Response } from "express";
+import { object } from "zod";
 // import { prisma, Prisma } from "@repo/product_db";
 type Product = Prisma.ProductGetPayload<{}>;
+export const ProductKeys = [
+  "name",
+  "shortDescription",
+  "description",
+  "price",
+  "sizes",
+  "colors",
+  "images",
+  "categorySlug",
+];
 export const createProduct = async (
   req: Request,
   res: Response
@@ -11,6 +22,33 @@ export const createProduct = async (
 
     if (!data) {
       res.status(400).json({ error: "Invalid product data" });
+      return;
+    }
+    const messingKeys = ProductKeys.filter((key) => !(key in data));
+    if (messingKeys.length > 0) {
+      res
+        .status(400)
+        .json({ error: `Missing required fields: ${messingKeys.join(", ")}` });
+      return;
+    }
+    const { colors, images } = data;
+    if (!Array.isArray(colors)) {
+      res.status(400).json({ error: "Colors must be an array" });
+      return;
+    }
+    if (!(typeof images === "object") || Array.isArray(images)) {
+      res.status(400).json({ error: "Images must be an object" });
+      return;
+    }
+    const messingColors = colors.filter((color) => {
+      console.log(color);
+      return !(color in images);
+    });
+    console.log(images);
+    if (messingColors.length > 0) {
+      res.status(400).json({
+        error: `Missing images for colors: ${messingColors.join(", ")}`,
+      });
       return;
     }
     const product = await prisma.product.create({ data });
@@ -28,7 +66,40 @@ export const updateProduct = async (
   res: Response
 ): Promise<void> => {
   try {
-    // Update product logic here
+    const { id } = req.params;
+    if (!id || !Number(id)) {
+      res.status(400).json({
+        success: false,
+        message: "Bad Request, id is required to update product",
+      });
+      return;
+    }
+    const product = await prisma.product.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!product) {
+      res.status(404).json({
+        success: false,
+        message: "product Not Found",
+      });
+      return;
+    }
+    const data: Prisma.ProductUpdateInput = req.body;
+    if (!data) {
+      res.status(400).json({
+        success: false,
+        message: "Bad Request, please provide data to update",
+      });
+      return;
+    }
+    const x = await prisma.product.update({
+      where: {
+        id: Number(id),
+      },
+      data,
+    });
     res.status(200).json({ message: "product updated successfully" });
   } catch (error) {
     console.error(error);
@@ -42,6 +113,31 @@ export const deleteProduct = async (
 ): Promise<void> => {
   try {
     // Delete product logic here
+    const { id } = req.params;
+    if (!id || !Number(id)) {
+      res.status(400).json({
+        success: false,
+        message: "Bad Request, id is required to delete product",
+      });
+      return;
+    }
+    const product = await prisma.product.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!product) {
+      res.status(404).json({
+        success: false,
+        message: "product Not Found",
+      });
+      return;
+    }
+    await prisma.product.delete({
+      where: {
+        id: Number(id),
+      },
+    });
     res.status(200).json({ message: "product deleted successfully" });
   } catch (error) {
     console.error(error);
@@ -55,7 +151,36 @@ export const getProducts = async (
 ): Promise<void> => {
   try {
     // Fetch products logic here
-    res.status(200).json({ data: [] });
+
+    const { sort, category, search, limit } = req.query;
+    const orderBy = (() => {
+      switch (sort as string) {
+        case "asc":
+          return { price: Prisma.SortOrder.asc };
+        case "desc":
+          return { price: Prisma.SortOrder.desc };
+        case "oldest":
+          return { createdAt: Prisma.SortOrder.asc };
+        case "newest":
+          return { createdAt: Prisma.SortOrder.desc };
+        default:
+          return undefined;
+      }
+    })();
+
+    const products = await prisma.product.findMany({
+      where: {
+        ...(category ? { category: { slug: category as string } } : {}),
+        name: {
+          contains: search as string,
+          mode: "insensitive",
+        },
+      },
+      orderBy,
+      take: limit ? Number(limit) : undefined,
+    });
+
+    res.status(200).json({ data: products });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch products" });
@@ -67,8 +192,26 @@ export const getProductById = async (
   res: Response
 ): Promise<void> => {
   try {
-    // Fetch single product by ID logic here
-    res.status(200).json({ data: {} });
+    // Fetch single product by ID logic heret
+    const { id } = req.params;
+    if (!id) {
+      res.status(401).json({ message: "please provide id" });
+      return;
+    }
+
+    const product = await prisma.product.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!product) {
+      res.status(404).json({
+        success: false,
+        message: "product Not Found",
+      });
+      return;
+    }
+    res.status(200).json({ data: { product } });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch product" });
